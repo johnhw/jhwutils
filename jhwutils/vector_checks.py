945902ef21d7ba29c100b0d3bf4d92ddd10f3502
John Williamson <johnhw@gmail.com>
2022-10-03 12:17:13 +0100

    Fix recursion check

--- jhwutils/vector_checks.py
+++ jhwutils/vector_checks.py
@@ -1,12 +1,15 @@
 import ast, inspect
 from textwrap import dedent
+
 ## From https://stackoverflow.com/questions/36662181/is-there-a-way-to-check-if-function-is-recursive-in-python
 from bdb import Bdb
 import sys
 import numpy as np
 
+
 class RecursionDetected(Exception):
     pass
+
 
 class RecursionDetector(Bdb):
     def do_clear(self, arg):
@@ -25,6 +28,7 @@ class RecursionDetector(Bdb):
     def user_return(self, frame, return_value):
         self.stack.remove(frame.f_code)
 
+
 def test_recursion(func):
     detector = RecursionDetector()
     detector.set_trace()
@@ -36,49 +40,50 @@ def test_recursion(func):
         return False
     finally:
         sys.settrace(None)
-        
+
+
 def check_vectorised(fn):
     tree = ast.parse(dedent(inspect.getsource(fn)))
     for node in ast.walk(tree):
-        if type(node)==ast.For:
+        if type(node) == ast.For:
             print("Hey, you used 'for'!")
             return False
-        if type(node)==ast.ListComp:
+        if type(node) == ast.ListComp:
             print("No list comprehensions, champ.")
             return False
-        if type(node)==ast.DictComp:
+        if type(node) == ast.DictComp:
             print("I know about dictionary comprehensions, too. No go.")
             return False
-        if type(node)==ast.SetComp:
+        if type(node) == ast.SetComp:
             print("Set comprehensions? Really?")
             return False
-        if type(node)==ast.comprehension:
+        if type(node) == ast.comprehension:
             print("Good try, but that's still iteration")
             return False
-        if type(node)==ast.While:
+        if type(node) == ast.While:
             print("No 'while' loops, buddy.")
-            return False        
+            return False
     return True
 
-def check_is_vectorised(f, vec_f, *args, **kwargs):    
+
+def check_is_vectorised(f, vec_f, *args, **kwargs):
     if not check_vectorised(vec_f):
         print("Not a vectorised function. Use NumPy.")
         return False
-    recursion = test_recursion(lambda :vec_f(*args, **kwargs))
-    if recursion:
-        print("Recursion won't fool me.")
-        return False
+    # recursion = test_recursion(lambda :vec_f(*args, **kwargs))
+    # if recursion:
+    #     print("Recursion won't fool me.")
+    #     return False
     expected = f(*args, **kwargs)
     actual = vec_f(*args, **kwargs)
-
-    
 
     if not np.allclose(expected, actual):
         print("Values don't match")
         return False
     return True
-        
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     import numpy as np
 
     def f(x):
@@ -86,28 +91,26 @@ if __name__=="__main__":
         for i in range(x):
             a.append(i)
         for i in range(x):
-            a[i] = a[i] * x    
+            a[i] = a[i] * x
         return np.array(a)
 
     def while_f(x):
         a = []
-        i = 0 
-        while i<x:        
+        i = 0
+        while i < x:
             a.append(i)
             i += 1
         for i in range(x):
-            a[i] = a[i] * x    
+            a[i] = a[i] * x
         return np.array(a)
 
     def lc_f(x):
-        return np.array([i*x for i in range(x)])
+        return np.array([i * x for i in range(x)])
 
     def vec_f(x):
-        return np.arange(x)*x
+        return np.arange(x) * x
 
-    assert check_is_vectorised(f, vec_f, 10) # OK
-    assert not check_is_vectorised(f, f, 10) # for
-    assert not check_is_vectorised(f, while_f, 10) # for
-    assert not check_is_vectorised(f, lc_f, 10) # for
-
-
+    assert check_is_vectorised(f, vec_f, 10)  # OK
+    assert not check_is_vectorised(f, f, 10)  # for
+    assert not check_is_vectorised(f, while_f, 10)  # for
+    assert not check_is_vectorised(f, lc_f, 10)  # for
