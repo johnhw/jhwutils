487cf0e072a0646d1292fa7a253087d0c34c33ef
John Williamson <johnhw@gmail.com>
2023-10-23 18:15:18 +0100

    Update notebook formatting

--- jhwutils/nbstripcode.py
+++ jhwutils/nbstripcode.py
@@ -82,6 +82,18 @@ def sha(s):
     """return the SHA1 hash of a string"""
     return sha1(s.encode("utf8")).hexdigest()
 
+def write_image(b64, fname):
+    """Write a base64-encoded image to a file"""
+    with open(fname, "wb") as f:
+        f.write(b64decode(b64))
+
+def get_attachment_path(nb_path, *args, extension="png"):
+    output_dir = nb_path.resolve().parent / "generated"
+    Path.mkdir(output_dir, exist_ok=True)
+    cell_id = sha(nb_path.name + "".join(str(s) for s in args))
+    return output_dir / f"attachment_{cell_id}.{extension}"
+    
+
 def extract_attachments(attachments, ix, nb_path):
     """Extract attachments from a cell.
     (i.e. inline base64-encoded images).
@@ -89,25 +101,41 @@ def extract_attachments(attachments, ix, nb_path):
     Expects the attachments to be a dictionary of dictionaries; a cell
     index and a path (used to create a unique filename).
 
-    Outputs images to nb_path/generated/attachment_{cell_id}_{fname}.
+    Outputs images to nb_path/generated/attachment_{cell_hash}.
     """
     # get the output path and cell id
-    output_dir = nb_path.resolve().parent / "generated"
-    Path.mkdir(output_dir, exist_ok=True)
-    cell_id = sha(nb_path.name + str(ix))
     output_names = {}
     # iterate over attachments
     for fname, attach in attachments.items():
         # note: we need to create an ID, because the filename is not unique
-        out_name = output_dir / f"attachment_{cell_id}_{fname}"
-        with open(out_name, "wb") as f:
-            for mimetype, b64 in attach.items():
-                attachment = b64decode(b64)
-                f.write(attachment)
+        
+        for mimetype, b64 in attach.items():            
+            extension = mimetype.split("/")[-1]
+            out_name = get_attachment_path(nb_path, ix, fname, extension=extension)        
+            write_image(b64, out_name)                                                    
         output_names[fname] = out_name
-        print(f"Extracted {fname} to {out_name}.")
+        click.echo(f"Extracted {fname} to {out_name}.")
     return output_names
 
+
+def extract_data(data, ix, j, nb_path):
+    """Extract inline data from a cell.
+    Each data item is a dictionary with a mimetype and a base64-encoded
+    string.
+
+    Outputs images to nb_path/generated/data_{cell_hash}.
+    """
+    output_names = []
+ 
+    for i, (mimetype, b64) in enumerate(data.items()):
+        mime, extension = mimetype.split("/")
+        if mime == "image":            
+            out_name = get_attachment_path(nb_path, ix, j, i, mimetype, extension=extension)
+            write_image(b64, out_name)
+            output_names.append(out_name)
+            click.echo(f"Extracted data to {out_name}.")
+    
+    return output_names
 
 def strip_code_nb(
     nb,
@@ -138,14 +166,37 @@ def strip_code_nb(
                             )
             cells.append(cell)
         # code cell
-        if cell.cell_type == "code":
-            if strip_outputs:
-                cell = strip_output_from_cell(cell)
-            if not strip_code:
-                cells.append(cell)
+        if cell.cell_type == "code":            
+            if not strip_code:                       
+                if strip_outputs:
+                    cell = strip_output_from_cell(cell)
+                
+
+                if not strip_outputs:
+                    # check for attached images in the data output     
+                    if "outputs" in cell:
+                        cell.source = strip_image(cell.source) 
+                        outs = cell["outputs"]
+                        cell["outputs"] = []
+                        for j,output in enumerate(outs):
+                            if "data" in output:                                                                
+                                images = extract_data(output["data"], ix, j, nb_path)                                
+                                if len(images)==0:
+                                    cell["outputs"].append(output)
+                                else:
+                                    # replace the attachment with a link
+                                    img_cell = cell.copy()
+                                    img_cell.cell_type = "markdown"
+                                    img_cell.source = ""
+                                    for out_name in images:
+                                        img_cell.source += f"![]({out_name})\n\n"
+                                    cells.append(img_cell)
+                cells.append(cell)                
+
             # check for "retain_img(...)" in this cell
+            # but only if images are enabled and we have already stripped the outputs
             match = re.search(r'retain_img\("([^\)]*)"\)', cell.source)
-            if match and not strip_images and retain_imgs:
+            if match and not strip_images and strip_outputs and retain_imgs:
                 img = match.group(1)
                 cell = cell.copy()
                 cell.cell_type = "markdown"
@@ -161,31 +212,29 @@ def strip_code_nb(
 @click.command()
 @click.argument("notebook_path", type=click.Path(exists=True))
 @click.option(
-    "--strip-images", is_flag=True, help="Strip images from the notebook", default=False
+    "--strip-images/--no-strip-images",  help="Strip images from the notebook", default=False
 )
 @click.option(
-    "--strip-code",
-    is_flag=True,
+    "--strip-code/--no-strip-code",
     help="Strip code cells from the notebook",
     default=True,
 )
 @click.option(
-    "--strip-output", is_flag=True, help="Strip output from the notebook", default=True
+    "--strip-output/--no-strip-output",  help="Strip output from the notebook", default=True
 )
 @click.option(
-    "--retain-attachments",
-    is_flag=True,
+    "--retain-attachments/--no-retain-attachments",
+
     help="Retain attachments in the notebook",
     default=True,
 )
 @click.option(
-    "--retain-imgs",
-    is_flag=True,
+    "--retain-imgs/--no-retain-imgs",
     help="Keep images with `retain_img()` in the code cells of a notebook",
     default=True,
 )
 @click.option(
-    "--markdeep", is_flag=True, help="Use Markdeep format for the output", default=True
+    "--markdeep/--no-markdeep", help="Use Markdeep format for the output", default=True
 )
 @click.option(
     "--css-file",
@@ -194,16 +243,20 @@ def strip_code_nb(
     default=None,
 )
 @click.option(
-    "--link-css",
-    is_flag=True,
+    "--link-css/--no-link-css",
     help="Link to CSS file instead of embedding",
     default=False,
 )
 @click.option(
-    "--link-markdeep",
-    is_flag=True,
+    "--link-markdeep/--no-link-markdeep",
     help="Link to Markdeep JS instead of embedding",
     default=False,
+)
+@click.option(
+    "--output",
+    type=click.Path(),
+    help="Output file",
+    default=None,
 )
 def convert(
     notebook_path,
@@ -216,6 +269,7 @@ def convert(
     css_file,
     link_css,
     link_markdeep,
+    output,
 ):
     """
     Convert a Jupyter notebook to a different format with specified options.
@@ -235,6 +289,7 @@ def convert(
     export_markdown(
         nb,
         notebook_path,
+        output,
         with_markdeep=markdeep,
         embed_markdeep=not link_markdeep,
         css_file=css_file,
@@ -284,7 +339,7 @@ def get_css(css_file=None, link=False):
 
 
 def export_markdown(
-    nb, path, with_markdeep=True, embed_markdeep=True, css_file=None, css_link=False
+    nb, path, output_path, with_markdeep=True, embed_markdeep=True, css_file=None, css_link=False
 ):
     markdown_exporter = MarkdownExporter()
     markdown_output, _ = markdown_exporter.from_notebook_node(nb)
@@ -296,12 +351,17 @@ def export_markdown(
         html_js = get_markdeep(embed_markdeep)
         markdown_output = header + html_css + markdown_output + html_js + "</body></html>"
 
-    stem = path.parent / path.stem
-    # split extension from path using pathlib
-    if with_markdeep:
-        new_md = str(stem) + ".md.html"
+    if output_path is None:    
+        stem = path.parent / path.stem
+
+        # split extension from path using pathlib
+        if with_markdeep:
+            new_md = str(stem) + ".md.html"
+        else:
+            new_md = str(stem) + ".md"
     else:
-        new_md = str(stem) + ".md"
+        new_md = output_path
+
     with io.open(new_md, "w", encoding="utf8") as f:
         f.write(markdown_output)
     print(f"Writing to {new_md}.")
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='jhwutils',
-      version='1.1',
+      version='1.2',
       description='Teaching support utilities',
       author='John H. Williamson',
       author_email='johnhw@gmail.com',
